5a90977371e38b824b6d02390805b399f025ed65
Michelle Olivieri <michelleo72@gmail.com>
2018-03-23 12:20:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,29 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\b[aeiouAEIOU]/)
+    true
+  else
+    false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+# text.scan(/\b(un)(\w+ing)\b/)
+text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+#text.scan(/^\w{5}\$/)
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  # text.scan(/\b[A-Z).+\p{P}?(?=\s|$))
+  
+  #   if text.match(/^[A-Z].+[\.!?]$/)
+  #     true
+  #   else
+  #     false
+# end
 end
 
 def valid_phone_number?(phone)
