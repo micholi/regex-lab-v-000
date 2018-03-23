5ffc062645d6a285f50208ae671adb9a27faddfc
Michelle Olivieri <michelleo72@gmail.com>
2018-03-23 12:39:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,25 +11,21 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-#text.scan(/^\w{5}\$/)
-text.scan(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-#  text.scan(/\b[A-Z).+\p{P}?(?=\s|$))
-
-     if text.match(/^[A-Z].+[\.!?]$/)
-       true
-     else
-       false
+  if text.match(/^[A-Z].+[\.!?]$/)
+    true
+  else
+    false
  end
 end
 
 def valid_phone_number?(phone)
   if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
-    #phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
     true
   else
     false
-end
+  end
 end
